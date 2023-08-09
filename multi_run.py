98d58df59a19d51f9fd86cdcd650681fbc3a0d10
Theieyrre <crizftfm@gmail.com>
2023-08-09 06:28:27 +0300

    Fixed order error in multi_run

--- multi_run.py
+++ multi_run.py
@@ -1,12 +1,13 @@
 import sys, os
 from PDFCreator import PDFCreator
 from glob import glob
+from natsort import natsorted
 
 
 def main():
     main_dir = sys.argv[1]
     save_dir = sys.argv[2]
-    for dir in glob(main_dir + "/*/"):
+    for dir in natsorted(glob(main_dir + "/*/")):
         filename = dir.split("\\")[-2] + ".pdf"
         filename = os.path.join(save_dir, filename)
         pdf_c = PDFCreator(filename)
